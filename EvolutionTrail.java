5d417ea7238412cf70064a7c764d42809d341b03
Showbizelephant <showbizelephant@gmail.com>
2015-06-04 09:34:47 -0700

    Buttons now work, Ev Traits work also

--- EvolutionTrail.java
+++ EvolutionTrail.java
@@ -22,19 +22,6 @@ public class EvolutionTrail extends Applet implements MouseListener
       int negRect6xco,negRect6yco,negRect6width,negRect6height;
       int nextButtonxco,nextButtonyco,nextButtonWidth,nextButtonHeight;
       
-      boolean posRect1Clicked = false;
-      boolean negRect1Clicked = false;
-      boolean posRect2Clicked = false;
-      boolean negRect2Clicked = false;
-      boolean posRect3Clicked = false;
-      boolean negRect3Clicked = false;
-      boolean posRect4Clicked = false;
-      boolean negRect4Clicked = false;
-      boolean posRect5Clicked = false;
-      boolean negRect5Clicked = false;
-      boolean posRect6Clicked = false;
-      boolean negRect6Clicked = false;
-      
       boolean mouseEntered;
       
       int room; //This determines which screen is showing (0 = Title screen 1 = Stats, 2 = Ev Traits,
@@ -118,7 +105,7 @@ public class EvolutionTrail extends Applet implements MouseListener
          if(room == 0){
             //paint some sort of title screen
          }
-         if(room == 1){
+         if(room == 2){//Stats Screen
             g.setColor(Color.blue);
             
             g.fillRect(posRect1xco,posRect1yco,posRect1width,posRect1height);
@@ -159,63 +146,28 @@ public class EvolutionTrail extends Applet implements MouseListener
             g.drawString("Disease Resistance: " + pop.DR, 60, 290);
             g.drawString("Hunting Efficiency: " + pop.HE, 60, 350);
             g.drawString("Extra Points: " + pop.extraPoints, 60, 386);
-         //this should probably go somewhere else
-            if(posRect1Clicked == true && pop.extraPoints > 0 && pop.St < 10){
-                  pop.St++;
-                  pop.extraPoints--;
-            }
-            if(negRect1Clicked == true && pop.St > 0){
-                  pop.St--;
-                  pop.extraPoints++;
-            }
-            if(posRect2Clicked == true && pop.extraPoints > 0 && pop.De < 10){
-                  pop.De++;
-                  pop.extraPoints--;
-            }
-            if(negRect2Clicked == true && pop.De > 0){
-                  pop.De--;
-                  pop.extraPoints++;
-            }
-            if(posRect3Clicked == true && pop.extraPoints > 0 && pop.Sp < 10){
-                  pop.Sp++;
-                  pop.extraPoints--;
-            }
-            if(negRect3Clicked == true && pop.Sp > 0){
-                  pop.Sp--;
-                  pop.extraPoints++;
-            }
-            if(posRect4Clicked == true && pop.extraPoints > 0 && pop.In < 10){
-                  pop.In++;
-                  pop.extraPoints--;
-            }
-            if(negRect4Clicked == true && pop.In > 0){
-                  pop.In--;
-                  pop.extraPoints++;
-            }
-            if(posRect5Clicked == true && pop.extraPoints > 0 && pop.DR < 5){
-                  pop.DR++;
-                  pop.extraPoints--;
-            }
-            if(negRect5Clicked == true && pop.DR > 0){
-                  pop.DR--;
-                  pop.extraPoints++;
-            }
-            if(posRect6Clicked == true && pop.extraPoints > 0 && pop.HE < 5){
-                  pop.HE++;
-                  pop.extraPoints--;
-            }
-            if(negRect6Clicked == true && pop.HE > 0){
-                  pop.HE--;
-                  pop.extraPoints++;
-            }
          }
-         else if(room == 2){
+         else if(room == 1){//Evolution Traits Screen
             g.setColor(Color.gray);//reusing prev rectangles to save space
             g.fillRect(nextButtonxco,nextButtonyco,nextButtonWidth,nextButtonHeight);
+            if(pop.PT == true)
+               g.setColor(Color.yellow);
             g.fillRect(negRect1xco,negRect1yco+10,negRect1width,negRect1height);
+            g.setColor(Color.gray);
+            if(pop.Sw == true)
+               g.setColor(Color.yellow);
             g.fillRect(negRect2xco,negRect2yco+10,negRect2width,negRect2height);
+            g.setColor(Color.gray);
+            if(pop.Om == true)
+               g.setColor(Color.yellow);
             g.fillRect(negRect3xco,negRect3yco+10,negRect3width,negRect3height);
+            g.setColor(Color.gray);
+            if(pop.WP == true)
+               g.setColor(Color.yellow);
             g.fillRect(negRect4xco,negRect4yco+10,negRect4width,negRect4height);
+            g.setColor(Color.gray);
+            if(pop.EC == true)
+               g.setColor(Color.yellow);
             g.fillRect(negRect5xco,negRect5yco+10,negRect5width,negRect5height);
             
             g.setColor(Color.black);
@@ -232,23 +184,7 @@ public class EvolutionTrail extends Applet implements MouseListener
             g.drawString("Start with more food and population.", 75, 260);
             g.drawString("Extra Chromosome", 60, 290);
             g.drawString("Gives you more stat points to use.", 75, 320);
-            g.drawString("Traits Left: " + pop.evPoints, 60, 386);
-            
-            if(negRect1Clicked == true){ //for some reason, this doesn't work as intended.
-               if(pop.PT == false && pop.evPoints > 0){
-                  pop.evPoints--;
-                  pop.PT = true;
-                  g.setColor(Color.yellow);
-                  g.fillRect(negRect1xco,negRect1yco+10,negRect1width,negRect1height);
-               }
-               else if(pop.PT == true && pop.evPoints < 2){
-                  pop.evPoints++;
-                  pop.PT = false;
-                  g.setColor(Color.gray);
-                  g.fillRect(negRect1xco,negRect1yco+10,negRect1width,negRect1height);
-               }
-            }
-     
+            g.drawString("Traits Left: " + pop.evPoints, 60, 386);   
          }
       }
       
@@ -270,98 +206,120 @@ public class EvolutionTrail extends Applet implements MouseListener
       {
          xpos = me.getX();
          ypos = me.getY();
-         if(room == 1){
-            if (xpos>posRect1xco && xpos<posRect1xco + posRect1width && ypos>posRect1yco && ypos<posRect1yco + posRect1height)  
-               posRect1Clicked = true; 
-            else   
-               posRect1Clicked = false;
-               
-            if (xpos>negRect1xco && xpos<negRect1xco + negRect1width && ypos>negRect1yco && ypos<negRect1yco + negRect1height)  
-               negRect1Clicked = true; 
-            else   
-               negRect1Clicked = false;  
-            
-            if (xpos>posRect2xco && xpos<posRect2xco + posRect2width && ypos>posRect2yco && ypos<posRect2yco + posRect2height)  
-               posRect2Clicked = true; 
-            else   
-               posRect2Clicked = false;
-               
-            if (xpos>negRect2xco && xpos<negRect2xco + negRect2width && ypos>negRect2yco && ypos<negRect2yco + negRect2height)  
-               negRect2Clicked = true; 
-            else   
-               negRect2Clicked = false;
-            
-            if (xpos>posRect3xco && xpos<posRect3xco + posRect3width && ypos>posRect3yco && ypos<posRect3yco + posRect3height)  
-               posRect3Clicked = true; 
-            else   
-               posRect3Clicked = false;
-               
-            if (xpos>negRect3xco && xpos<negRect3xco + negRect3width && ypos>negRect3yco && ypos<negRect3yco + negRect3height)  
-               negRect3Clicked = true; 
-            else   
-               negRect3Clicked = false; 
-            
-            if (xpos>posRect4xco && xpos<posRect4xco + posRect4width && ypos>posRect4yco && ypos<posRect4yco + posRect4height)  
-               posRect4Clicked = true; 
-            else   
-               posRect4Clicked = false;
-               
-            if (xpos>negRect4xco && xpos<negRect4xco + negRect4width && ypos>negRect4yco && ypos<negRect4yco + negRect4height)  
-               negRect4Clicked = true; 
-            else   
-               negRect4Clicked = false;    
-            
-            if (xpos>posRect5xco && xpos<posRect5xco + posRect5width && ypos>posRect5yco && ypos<posRect5yco + posRect5height)  
-               posRect5Clicked = true; 
-            else   
-               posRect5Clicked = false;
-               
-            if (xpos>negRect5xco && xpos<negRect5xco + negRect5width && ypos>negRect5yco && ypos<negRect5yco + negRect5height)  
-               negRect5Clicked = true; 
-            else   
-               negRect5Clicked = false;   
-            
-            if (xpos>posRect6xco && xpos<posRect6xco + posRect6width && ypos>posRect6yco && ypos<posRect6yco + posRect6height)  
-               posRect6Clicked = true; 
-            else   
-               posRect6Clicked = false;
-               
-            if (xpos>negRect6xco && xpos<negRect6xco + negRect6width && ypos>negRect6yco && ypos<negRect6yco + negRect6height)  
-               negRect6Clicked = true; 
-            else   
-               negRect6Clicked = false;             
+         if(room == 2){
+            if (xpos>posRect1xco && xpos<posRect1xco + posRect1width && ypos>posRect1yco && ypos<posRect1yco + posRect1height && pop.extraPoints > 0 && pop.St < 10){ 
+               pop.St++;
+               pop.extraPoints--; 
+            }   
+            if (xpos>negRect1xco && xpos<negRect1xco + negRect1width && ypos>negRect1yco && ypos<negRect1yco + negRect1height && pop.De > 0){ 
+               pop.St--;
+               pop.extraPoints++;
+            }           
+            if (xpos>posRect2xco && xpos<posRect2xco + posRect2width && ypos>posRect2yco && ypos<posRect2yco + posRect2height && pop.extraPoints > 0 && pop.De < 10){  
+               pop.De++;
+               pop.extraPoints--;
+            }
+            if (xpos>negRect2xco && xpos<negRect2xco + negRect2width && ypos>negRect2yco && ypos<negRect2yco + negRect2height && pop.De > 0){  
+               pop.De--;
+               pop.extraPoints++;
+            }
+            if (xpos>posRect3xco && xpos<posRect3xco + posRect3width && ypos>posRect3yco && ypos<posRect3yco + posRect3height && pop.extraPoints > 0 && pop.Sp < 10){  
+               pop.Sp++;
+               pop.extraPoints--;
+            }  
+            if (xpos>negRect3xco && xpos<negRect3xco + negRect3width && ypos>negRect3yco && ypos<negRect3yco + negRect3height && pop.Sp > 0){  
+               pop.Sp--;
+               pop.extraPoints++;
+            }
+            if (xpos>posRect4xco && xpos<posRect4xco + posRect4width && ypos>posRect4yco && ypos<posRect4yco + posRect4height && pop.extraPoints > 0 && pop.In < 10){  
+               pop.In++;
+               pop.extraPoints--;
+            }   
+            if (xpos>negRect4xco && xpos<negRect4xco + negRect4width && ypos>negRect4yco && ypos<negRect4yco + negRect4height && pop.In > 0){     
+               pop.In--;
+               pop.extraPoints++;
+            }
+            if (xpos>posRect5xco && xpos<posRect5xco + posRect5width && ypos>posRect5yco && ypos<posRect5yco + posRect5height && pop.extraPoints > 0 && pop.DR < 5){  
+               pop.DR++;
+               pop.extraPoints--;
+            }  
+            if (xpos>negRect5xco && xpos<negRect5xco + negRect5width && ypos>negRect5yco && ypos<negRect5yco + negRect5height && pop.DR > 0){   
+               pop.DR--;
+               pop.extraPoints++;
+            }
+            if (xpos>posRect6xco && xpos<posRect6xco + posRect6width && ypos>posRect6yco && ypos<posRect6yco + posRect6height && pop.extraPoints > 0 && pop.HE < 5){  
+               pop.HE++;
+               pop.extraPoints--;
+            }  
+            if (xpos>negRect6xco && xpos<negRect6xco + negRect6width && ypos>negRect6yco && ypos<negRect6yco + negRect6height && pop.HE > 0){ 
+               pop.HE--;
+               pop.extraPoints++;
+            }    
             if (xpos>nextButtonxco && xpos<nextButtonxco+nextButtonWidth && ypos>nextButtonyco && ypos<nextButtonyco+nextButtonHeight)
                room++;
          }
          
-         else if(room == 2){
-            if (xpos>negRect1xco && xpos<negRect1xco + negRect1width && ypos>negRect1yco+10 && ypos<negRect1yco+10 + negRect1height)  
-               negRect1Clicked = true; 
-            else   
-               negRect1Clicked = false;  
-            
+         else if(room == 1){
+            if (xpos>negRect1xco && xpos<negRect1xco + negRect1width && ypos>negRect1yco+10 && ypos<negRect1yco+10 + negRect1height){  
+               if(pop.PT == false && pop.evPoints > 0){
+                  pop.evPoints--;
+                  pop.PT = true;
+               }
+               else if(pop.PT == true && pop.evPoints < 2){
+                  pop.evPoints++;
+                  pop.PT = false;
+               }
+            }
             if (xpos>negRect2xco && xpos<negRect2xco + negRect2width && ypos>negRect2yco+10 && ypos<negRect2yco+10 + negRect2height)  
-               negRect2Clicked = true; 
-            else   
-               negRect2Clicked = false;
-            
-            if (xpos>negRect3xco && xpos<negRect3xco + negRect3width && ypos>negRect3yco+10 && ypos<negRect3yco+10 + negRect3height)  
-               negRect3Clicked = true; 
-            else   
-               negRect3Clicked = false; 
-               
-            if (xpos>negRect4xco && xpos<negRect4xco + negRect4width && ypos>negRect4yco+10 && ypos<negRect4yco+10 + negRect4height)  
-               negRect4Clicked = true; 
-            else   
-               negRect4Clicked = false;             
-               
-            if (xpos>negRect5xco && xpos<negRect5xco + negRect5width && ypos>negRect5yco+10 && ypos<negRect5yco+10 + negRect5height)  
-               negRect5Clicked = true; 
-            else   
-               negRect5Clicked = false;
-         }      
+               if(pop.Sw == false && pop.evPoints > 0){
+                  pop.evPoints--;
+                  pop.Sw = true;
+               }
+               else if(pop.Sw == true && pop.evPoints < 2){
+                  pop.evPoints++;
+                  pop.Sw = false;
+               }
+            }
+            if (xpos>negRect3xco && xpos<negRect3xco + negRect3width && ypos>negRect3yco+10 && ypos<negRect3yco+10 + negRect3height){
+               if(pop.Om == false && pop.evPoints > 0){
+                  pop.evPoints--;
+                  pop.Om = true;;
+               }
+               else if(pop.Om == true && pop.evPoints < 2){
+                  pop.evPoints++;
+                  pop.Om = false;
+               }
+            }     
+            if (xpos>negRect4xco && xpos<negRect4xco + negRect4width && ypos>negRect4yco+10 && ypos<negRect4yco+10 + negRect4height){
+               if(pop.WP == false && pop.evPoints > 0){
+                  pop.evPoints--;
+                  pop.WP = true;
+                  pop.size += 100;
+                  pop.food += 200;
+               }
+               else if(pop.WP == true && pop.evPoints < 2){
+                  pop.evPoints++;
+                  pop.WP = false;
+                  pop.size -= 100;
+                  pop.food -= 200;
+               }
+            }               
+            if (xpos>negRect5xco && xpos<negRect5xco + negRect5width && ypos>negRect5yco+10 && ypos<negRect5yco+10 + negRect5height){
+               if(pop.EC == false && pop.evPoints > 0){
+                  pop.evPoints--;
+                  pop.EC = true;
+                  pop.extraPoints += 5;
+               }
+               else if(pop.EC == true && pop.evPoints < 2){
+                  pop.evPoints++;
+                  pop.EC = false;
+                  pop.extraPoints -= 5;
+               }
+            } 
+            if (xpos>nextButtonxco && xpos<nextButtonxco+nextButtonWidth && ypos>nextButtonyco && ypos<nextButtonyco+nextButtonHeight)
+               room++;
          repaint();
-         me.consume();            
+         me.consume();          
       }
        
       public void mouseEntered (MouseEvent me) {} 
